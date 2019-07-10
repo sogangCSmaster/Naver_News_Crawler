408e7cbdace6eb961e8c6dcc84af0eb9bb52b934
redbird <cs20131575@u.sogang.ac.kr>
2019-07-11 01:05:50 +0900

    added title, publishedDate, thumbnail, content extracting script

--- crawler.py
+++ crawler.py
@@ -10,7 +10,7 @@ def crawl_url(sid2, date, page):
     req = requests.get(url)
     status = req.status_code
     if status!= 200:
-        return print("status code :", status)
+        return print("Error, status code :", status)
 
     print("url fetched!")
     html = req.text
@@ -26,4 +26,42 @@ def crawl_url(sid2, date, page):
         data.append({'press': press, 'url': url})
     
     print("returned url datas!")
+    return data
+
+def crawl_news(url):
+    data = {}
+    req = requests.get(url)
+    status = req.status_code
+    if status!= 200:
+        return print("Error, status code :", status)
+
+    print("url fetched!")
+    html = req.text
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    for useless in soup(['script', 'style']):
+        useless.extract()
+
+    
+    thumbnail = soup.find('meta', property='og:image')
+    thumbnail = thumbnail['content']
+    title = soup.find('meta', property='og:title')
+    title = title['content']
+    publishedAt = soup.find('span', {'class': 't11'})
+    publishedAt = publishedAt.text
+    content = soup.find('div', id='articleBodyContents')
+
+
+    for imgTag in content.find_all('span', {'class': 'end_photo_org'}):
+        imgTag.decompose()
+    for aTag in content.find_all('a'):
+        aTag.decompose()
+    
+    content = content.text.strip()
+    data['title'] = title
+    data['content'] = content
+    data['thumbnail'] = thumbnail
+    data['publishedAt'] = publishedAt
+
+
     return data
--- example.py
+++ example.py
@@ -1,8 +1,12 @@
-from crawler import crawl_url
-
-
+from crawler import crawl_url, crawl_news
 
 # 금융(259) 카테고리 2019년 07월 03일 1페이지 url 수집
 urlDatas = crawl_url(259, '20190703', 1)
-print('작성 언론사 : ' + urlDatas[0]['press'])
-print('url : ' + urlDatas[0]['url'])
+print('작성 언론사 : ' + urlDatas[1]['press'])
+print('url : ' + urlDatas[1]['url'])
+url = urlDatas[1]['url']
+
+contentData = crawl_news(url)
+print(contentData['title'])
+print(contentData['publishedAt'])
+print(contentData['thumbnail'])
